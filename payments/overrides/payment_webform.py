6844ab53d009231dcf5756fefe782c27bf4f3cdc
Corentin Forler <8860073-cforler_dokos@users.noreply.gitlab.com>
2023-02-10 11:37:48 +0100

    feat: Refactor web form fields

--- payments/overrides/payment_webform.py
+++ payments/overrides/payment_webform.py
@@ -12,22 +12,35 @@ class PaymentWebForm(WebForm):
 
 		if getattr(self, "accept_payment", False):
 			self.validate_payment_amount()
-			self.validate_gateway_doc_field()
+			self.validate_currency()
+			self.validate_ref_doctype_fields()
 			self.clear_amount_if_fetching_from_field()
 			self.clear_currency_if_fetching_from_field()
 
-	def validate_gateway_doc_field(self):
+	def validate_ref_doctype_fields(self):
 		meta = frappe.get_meta(self.doc_type)
-		if not meta.has_field("payment_gateway"):
-			frappe.throw(_(
-				"The selected document type ({0}) must contain a `{1}` field. This field is used to store the payment gateway used for the payment made when the form is submitted."
-			).format(_(self.doc_type), "payment_gateway"))
+		required_fields = ["payment_gateway"]
+		for field in required_fields:
+			if not meta.has_field(field):
+				frappe.throw(_(
+					"The selected document type ({0}) must contain a `{1}` field. This field is used to store data when the form is submitted. Required fields are: {2}"
+				).format(_(self.doc_type), field, ", ".join(required_fields)))
 
 	def validate_payment_amount(self):
+		if not self.can_require_immediate_payment():
+			return  # no need to validate amount if not accepting immediate payment
 		if self.amount_based_on_field and not self.amount_field:
 			frappe.throw(_("Please select a Amount Field."))
 		elif not self.amount_based_on_field and not flt(self.amount) > 0:
 			frappe.throw(_("Amount must be greater than 0."))
+
+	def validate_currency(self):
+		if not self.can_require_immediate_payment():
+			return  # no need to validate currency if not accepting immediate payment
+		if self.currency_based_on_field and not self.currency_field:
+			frappe.throw(_("Please select a {0} Field.").format(_("Currency")))
+		elif not self.currency_based_on_field and not self.currency:
+			frappe.throw(_("The field {0} is mandatory").format(_("Currency")))
 
 	def clear_amount_if_fetching_from_field(self):
 		if self.amount_based_on_field:
@@ -38,9 +51,9 @@ class PaymentWebForm(WebForm):
 			self.currency = None
 
 	PAYMENT_TYPES = {
-		"Single payment": "immediate",
-		"Setup for automatic payments": "offline",
-		"Immediate payment and setup for automatic payments": "immediate+offline",
+		"Immediate payment": "immediate",
+		"Automatic payments": "offline",
+		"Initial payment followed by automatic payments": "immediate+offline",
 	}
 
 	def get_payment_type(self):
@@ -91,6 +104,10 @@ class PaymentWebForm(WebForm):
 			return doc.get(self.amount_field)
 		return self.amount
 
+	def can_require_immediate_payment(self):
+		payment_type = self.get_payment_type()
+		return payment_type in ("immediate", "immediate+offline")
+
 	def get_payment_gateway_url(self, doc):
 		payment_type = self.get_payment_type()
 		if not payment_type:
@@ -105,7 +122,7 @@ class PaymentWebForm(WebForm):
 			title = _("Payment for {0} {1}").format(_(doc.doctype), doc.name)
 
 		# Get amount and check it is valid
-		will_pay_immediately = payment_type in ("immediate", "immediate+offline")
+		will_pay_immediately = self.can_require_immediate_payment()
 		amount = None
 		currency = None
 		if will_pay_immediately:
